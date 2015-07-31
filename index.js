cc30af2a709cd440747a885003477b80b51f19b7
Berto Yáñez <berto@ber.to>
2015-07-31 23:28:50 +0200

    Better generation

--- index.js
+++ index.js
@@ -9,34 +9,74 @@ var con1 = [{
         f: 'unha '
     }
 }, {
-    m: ', conceptualmente é unha derivación ',
+    m: ' conceptualmente é unha derivación ',
     g: {
         m: 'do ',
         f: 'da ',
     }
+}, {
+    m: ' pode analizarse dende o prisma ',
+    g: {
+        m: 'do ',
+        f: 'da ',
+    }
+}, {
+    m: ' é unha extensión ',
+    g: {
+        m: 'do ',
+        f: 'da ',
+    }
+}, {
+    m: ', que fermosa metáfora ',
+    g: {
+        m: 'do ',
+        f: 'da ',
+    }
+}, {
+    m: ', debería estudarse como ',
+    g: {
+        m: 'un ',
+        f: 'unha ',
+    }
+
 }];
 
-var con2 = [{
+var con2 = [
+{
+    m: ', sempre '
+},{
     m: ', pero '
 }, {
     m: ', dende o punto de vista ',
     g: 'm'
+},{
+    m: ', sutilmente '
+}, {
+    m: ', en parte '
+}, {
+    m: ', sen dúbida '
 }];
 
 var con3 = [{
     m: ' e '
 }, {
-    m: ' e nunca '
+    m: ', nunca '
 }, {
     m: ' e con influenza ',
     g: 'f'
+}, {
+    m: ' máis '
+}, {
+    m: ', certamente '
+}, {
+    m: ', abertamente '
 }];
 
-var getSubSet = function(gender) {
+var getSubSet = function(classes) {
     'use strict';
 
     return _.filter(words, function(word) {
-        return word.gender === gender;
+        return _.intersection(classes, word.classes).length > 0;
     });
 };
 
@@ -50,13 +90,13 @@ var build = function() {
 
     var c1 = _.sample(con1);
 
-    var name = _.sample(getSubSet(nameGender));
+    var name = _.sample(getSubSet([nameGender]));
 
     var c2 = _.sample(con2);
     var c3 = _.sample(con3);
 
-    var ad = _.sample(getSubSet('a'));
-    var ad2 = _.sample(getSubSet('a'));
+    var ad = _.sample(getSubSet(['a']));
+    var ad2 = _.sample(getSubSet(['a']));
 
     var result =
         'O ' +
@@ -66,7 +106,8 @@ var build = function() {
         name.word + c2.m +
         ((c2.g || nameGender) === 'f' ? (ad.feminine ? ad.feminine : ad.word) : ad.word) +
         c3.m +
-        ((c3.g || nameGender) === 'f' ? (ad2.feminine ? ad2.feminine : ad2.word) : ad2.word);
+        ((c3.g || nameGender) === 'f' ? (ad2.feminine ? ad2.feminine : ad2.word) : ad2.word) +
+        '.';
 
     return result;
 };
